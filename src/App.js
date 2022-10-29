387e0da86b94b0bd6fa09dd50ae1ef0e9883c8c1
07arif <ai.arif0711@gmail.com>
2022-10-29 20:38:37 +0600

    Router setup, error page setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './components/Home';
+import Main from './components/Main';
+import Books from './components/Books';
+import About from './components/About'
 
+
+import ErrorPage from './components/ErrorPage';
 const App = () => {
+  const router = createBrowserRouter([
+    {
+      path:'/',
+      element:<Main></Main>,
+      errorElement:<ErrorPage></ErrorPage>,
+      children:[
+        {
+          path:'/',
+          element:<Home></Home>
+        },
+        {
+          path:'/home',
+          element:<Home></Home>
+        },
+        {
+          path:'/book',
+          element:<Books></Books>
+        },
+        {
+          path:'/about',
+          element:<About></About>
+        }
+        
+      ]
+    }
+  ])
   return (
     <div>
-      <h2>Hello</h2>
+     <RouterProvider router={router}></RouterProvider>
     </div>
   );
 };
